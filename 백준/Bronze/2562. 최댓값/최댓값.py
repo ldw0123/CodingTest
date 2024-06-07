854018f624bddfc5b452cc00004bcc0080c3eba4
DW <ldw0123@gmail.com>
2024-06-08 00:02:54 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,8 +1,6 @@
-numbers = []
-
-for _ in range(9):
-    i = int(input())
-    numbers.append(i)
+# List Comprehension으로 작성
+# Comprehension 표현식: [ 실행 문장 for 변수 in 리스트 ]
+numbers = [int(input()) for _ in range(9)]
 
 print(max(numbers))
 print(numbers.index(max(numbers)) + 1)
